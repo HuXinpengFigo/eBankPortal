64b168f2370c878a3541c946c5834f3c1d3f29c6
Delic Xinpeng HU <xinpenghu@astri.org>
2023-04-27 17:06:11 +0800

    Add CI/CD and docker-compose file

--- src/test/java/com/xinpenghu/ebankportal/EBankPortalApplicationTests.java
+++ src/test/java/com/xinpenghu/ebankportal/EBankPortalApplicationTests.java
@@ -17,51 +17,51 @@ import java.util.Optional;
 
 @SpringBootTest
 class EBankPortalApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//	@Autowired
-//	private UserKafkaProducer userKafkaProducer;
-//
-//	@Test
-//	void testKafka() {
-//		User user = new User("figohxp@example.com", "123456");
-//		userKafkaProducer.sendMessage(user);
-//	}
-//
-//	/*
-//	 * @test :- testMongo
-//	 * @Description :- Get  User Based on Email or Id
-//	 * @params:-
-//	 * */
-//	@Autowired
-//	private UserRepository userRepository;
-//	@Test
-//	void testMongo() {
-//		String email = "figohxp@example.com";
-//		String id = "6441f088c5b64a73c723dcd1";
-//		Optional<User> optionalUser = userRepository.findUserByEmail(email);
-//		System.out.println(optionalUser);
-//		optionalUser = userRepository.findById(id);
-//		System.out.println(optionalUser);
-//	}
-//
-//
-//	@Test
-//	void testExchangeAPI() throws JSONException {
-//		String url = "https://v6.exchangerate-api.com/v6/1af5924cda5c9b070747a158/latest/HKD";
-//		RestTemplate restTemplate = new RestTemplate();
-//		String result = restTemplate.getForObject(url, String.class);
-//
-//		JSONObject jsonObject = new JSONObject(result);
-//
-//		String exchangeRateString = jsonObject.getString("conversion_rates");
-//		Gson gson = new Gson();
-//		ExchangeRate exchangeRate = gson.fromJson(exchangeRateString, ExchangeRate.class);
-//
-//		System.out.println(exchangeRate.toString());
-//	}
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Autowired
+	private UserKafkaProducer userKafkaProducer;
+
+	@Test
+	void testKafka() {
+		User user = new User("figohxp@example.com", "123456");
+		userKafkaProducer.sendMessage(user);
+	}
+
+	/*
+	 * @test :- testMongo
+	 * @Description :- Get  User Based on Email or Id
+	 * @params:-
+	 * */
+	@Autowired
+	private UserRepository userRepository;
+	@Test
+	void testMongo() {
+		String email = "figohxp@example.com";
+		String id = "6441f088c5b64a73c723dcd1";
+		Optional<User> optionalUser = userRepository.findUserByEmail(email);
+		System.out.println(optionalUser);
+		optionalUser = userRepository.findById(id);
+		System.out.println(optionalUser);
+	}
+
+
+	@Test
+	void testExchangeAPI() throws JSONException {
+		String url = "https://v6.exchangerate-api.com/v6/1af5924cda5c9b070747a158/latest/HKD";
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(url, String.class);
+
+		JSONObject jsonObject = new JSONObject(result);
+
+		String exchangeRateString = jsonObject.getString("conversion_rates");
+		Gson gson = new Gson();
+		ExchangeRate exchangeRate = gson.fromJson(exchangeRateString, ExchangeRate.class);
+
+		System.out.println(exchangeRate.toString());
+	}
 
 }
